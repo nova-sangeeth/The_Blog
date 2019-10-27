183bc72d50f356983478c11327dae347d0a2bedd
nova-sangeeth <54772424+nova-sangeeth@users.noreply.github.com>
2019-10-28 00:42:29 +0530

    comments and spell checks

--- Blog_Application/models.py
+++ Blog_Application/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class posts(models.Model):
     title = models.CharField(max_length=256)
-# the bottom declare funtion is mainly used for showing the content present in the object declare above.
+# the bottom declared funtion is mainly used for showing the content present in the object declared above.
     def __str__(self):
         return self.title
