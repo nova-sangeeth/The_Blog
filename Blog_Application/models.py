8254974da528b5b2763acadf384afa1191371631
nova-sangeeth <novasangeeth@outlook.com>
2021-04-18 00:04:53 +0530

    Changes to the models to supress the warning for auto generation of primary keys in the table.

--- Blog_Application/models.py
+++ Blog_Application/models.py
@@ -2,16 +2,22 @@ from django.db import models
 from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
+
 class Post(models.Model):
+    post_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=256)
     description = models.TextField()
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     image = models.ImageField()
     slug = models.SlugField()
+
     def __str__(self):
         return self.title
+
+
 class Author(models.Model):
-    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+    author_id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     email = models.EmailField()
     cellphone_num = models.IntegerField()
 
